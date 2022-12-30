bbdb11806e952a4a273409ef664b22dced168c36
Archi903 <artpele@yandex.ru>
2022-12-30 19:18:02 +0300

    OOP2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import transport.Automobile;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Automobile[] automobiles = {
+                new Automobile("Lada", "Granta", 2021,
+                        "России", "", 1.7, "", "",
+                        "", 0)};
+        for (Automobile value : automobiles) {
+            System.out.println(value);
+
+        }
     }
 }
